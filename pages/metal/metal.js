ee96979b20752d0a1756e167daff77eb6f31780d
Jombo-k <994249582@qq.com>
2018-07-25 19:16:33 +0800

    演出列表点击进去详情静态页面完成

--- pages/metal/metal.js
+++ pages/metal/metal.js
@@ -1,18 +1,33 @@
 // pages/metal/metal.js
+var dateTimePicker = require('../../lib/dateTimePicker.js');
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    showModalStatus: false,
+    dateTimeArray1: null,
+    dateTime1: null
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    // 获取完整的年月日 时分秒，以及默认显示的数组
+    var obj = dateTimePicker.dateTimePicker(this.data.startYear, this.data.endYear);
+    var obj1 = dateTimePicker.dateTimePicker(this.data.startYear, this.data.endYear);
+    // 精确到分的处理，将数组的秒去掉
+    var lastArray = obj1.dateTimeArray.pop();
+    var lastTime = obj1.dateTime.pop();
+
+    this.setData({
+      dateTime: obj.dateTime,
+      dateTimeArray: obj.dateTimeArray,
+      dateTimeArray1: obj1.dateTimeArray,
+      dateTime1: obj1.dateTime
+    });
   },
 
   /**
@@ -62,5 +77,110 @@ Page({
    */
   onShareAppMessage: function () {
   
+  },
+
+
+  //点击添加
+  powerDrawer: function (e) {
+    var currentStatu = e.currentTarget.dataset.statu;
+    this.util(currentStatu)
+  },
+  util: function (currentStatu) {
+    let _self = this
+    /* 动画部分 */
+    // 第1步：创建动画实例   
+    var animation = wx.createAnimation({
+      duration: 200,  //动画时长  
+      timingFunction: "linear", //线性  
+      delay: 0  //0则不延迟  
+    })
+    // 第2步：这个动画实例赋给当前的动画实例  
+    this.animation = animation
+    // 第3步：执行第一组动画  
+    animation.opacity(0).rotateX(-100).step()
+    // 第4步：导出动画对象赋给数据对象储存  
+    this.setData({
+      animationData: animation.export()
+    })
+    // 第5步：设置定时器到指定时候后，执行第二组动画  
+    setTimeout(function () {
+      // 执行第二组动画  
+      animation.opacity(1).rotateX(0).step()
+      // 给数据对象储存的第一组动画，更替为执行完第二组动画的动画对象  
+      this.setData({
+        animationData: animation
+      })
+      //关闭  
+      if (currentStatu == "close") {
+        // _self.addList()
+        this.setData(
+          {
+            showModalStatus: false
+          }
+        )
+      }
+    }.bind(this), 200)
+    // 显示  
+    if (currentStatu == "open") {
+      this.setData(
+        {
+          showModalStatus: true
+        }
+      )
+    }
+  },
+  //弹出框
+  toastSuccess(title) {
+    wx.showToast({
+      title: title,
+      icon: 'success',
+      duration: 2000
+    })
+  },
+  //上传图片
+  addImage () {
+    wx.chooseImage({
+      count: 1, // 默认9
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        console.log(res)
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+        // var tempFilePaths = res.tempFilePaths;
+        // wx.uploadFile({
+        //   url: 'https://...',      //此处换上你的接口地址
+        //   filePath: tempFilePaths[0],
+        //   name: 'img',
+        //   header: {
+        //     "Content-Type": "multipart/form-data",
+        //     'accept': 'application/json',
+        //     'Authorization': 'Bearer ..'    //若有token，此处换上你的token，没有的话省略
+        //   },
+        //   formData: {
+        //     'user': 'test'  //其他额外的formdata，可不写
+        //   },
+        //   success: function (res) {
+        //     var data = res.data;
+        //     console.log('data');
+        //   },
+        //   fail: function (res) {
+        //     console.log('fail');
+
+        //   },
+        // })
+      }
+    })
+  },
+  changeDateTime1(e) {
+    this.setData({ dateTime1: e.detail.value });
+  },
+  changeDateTimeColumn1(e) {
+    var arr = this.data.dateTime1, dateArr = this.data.dateTimeArray1;
+    arr[e.detail.column] = e.detail.value;
+    dateArr[2] = dateTimePicker.getMonthDay(dateArr[0][arr[0]], dateArr[1][arr[1]]);
+    this.setData({
+      dateTimeArray1: dateArr,
+      dateTime1: arr
+    })
   }
 })
